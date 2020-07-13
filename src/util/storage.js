f2da61a335f6d21220fcd6d58cea178c30eaf640
AuntlonglegsLI <441434807@qq.com>
2020-07-13 21:15:01 +0800

    本地存储

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,10 +1,11 @@
-// 获取用户历史
-export const getItem = (msg) => {
-    return JSON.parse(localStorage.getItem(msg))
-}
+export const getItem = name => {
+    return JSON.parse(window.localStorage.getItem(name));
+};
 
-// 更新用户历史
-export const setItem = (msg) => {
-    return localStorage.setItem('search-histories', msg)
-}
+export const setItem = (name, value) => {
+    return window.localStorage.setItem(name, JSON.stringify(value));
+};
 
+export const removeItem = name => {
+    window.localStorage.removeItem(name);
+};
--- src/views/search/Index.vue
+++ src/views/search/Index.vue
@@ -1,49 +1,43 @@
 <template>
   <div class="search">
-    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
-      <!-- 搜索框 -->
-      <form action="/">
-        <van-search
-          v-model="value"
-          show-action
-          background="#3296fa"
-          placeholder="请输入搜索关键词"
-          @search="onSearch"
-          @cancel="onCancel"
-        >
-          <div slot="action" @click="onSearch(value)">搜索</div>
-        </van-search>
-      </form>
-      <!-- /搜索框 -->
+    <!-- 搜索框 -->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        background="#3296fa"
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+        @input="onSearchInput"
+      >
+        <div slot="action" @click="onSearch">搜索</div>
+      </van-search>
+    </form>
+    <!-- /搜索框 -->
 
-      <!-- 搜索联想建议自动补全 -->
-      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-        <van-cell
-          v-for="(item,index) in resultList"
-          :key="index"
-          icon="search"
-          @click="onSearch(item)"
-        >
-          <!-- 我们要把 item 处理成带有高亮的字符串 -->
-          <!-- 过滤器：专门用于文本格式化，但是它只能用在 {{}} 和 v-bind 中 -->
-          <div slot="title" v-html="highlight(item)"></div>
-        </van-cell>
-      </van-list>
-      <!-- /搜索联想建议自动补全 -->
+    <!-- 搜索联想建议自动补全 -->
+    <van-cell-group>
+      <van-cell icon="search" :key="index" v-for="(item,index) in resultList">
+        <!-- 我们要把 item 处理成带有高亮的字符串 -->
+        <!-- 过滤器：专门用于文本格式化，但是它只能用在 {{}} 和 v-bind 中 -->
+        <div slot="title" v-html="highlight(item)"></div>
+      </van-cell>
+    </van-cell-group>
+    <!-- /搜索联想建议自动补全 -->
 
-      <!-- 搜索历史记录 -->
-      <van-cell-group>
-        <van-cell title="历史记录">
-          <span>全部删除</span>&nbsp;&nbsp;
-          <span>完成</span>
-          <van-icon name="delete" />
-        </van-cell>
-        <van-cell v-for="item in searchHistory" :key="item" :title="item" @click="onSearch(item)">
-          <van-icon name="close" />
-        </van-cell>
-      </van-cell-group>
-      <!-- /搜索历史记录 -->
-    </van-pull-refresh>
+    <!-- 搜索历史记录 -->
+    <van-cell-group>
+      <van-cell title="历史记录">
+        <span>全部删除</span>&nbsp;&nbsp;
+        <span>完成</span>
+        <van-icon name="delete" />
+      </van-cell>
+      <van-cell v-for="(item2,index2) in searchHistory" :key="index2" :title="item" @click="onSearch(item2)">
+        <van-icon name="close" />
+      </van-cell>
+    </van-cell-group>
+    <!-- /搜索历史记录 -->
   </div>
 </template>
 
@@ -54,36 +48,21 @@ import { setItem, getItem } from "@/util/storage";
 export default {
   data() {
     return {
-      value: "",
+      searchText: "",
       resultList: [],
-      loading: false,
-      finished: false,
-      refreshing: false,
       searchHistory: getItem("search-histories") || []
     };
   },
-  watch: {
-    value: {
-      handler(newVal, oldVal) {
-        this.onSearchInput(newVal.trim());
-      },
-      deep: true,
-      immediate: true
-    }
-  },
   methods: {
     // 获取搜索联想 debounce防抖
-    onSearchInput: debounce(async function(keyWord) {
-      if (keyWord) {
-        try {
-          let data = await suggestion(keyWord);
-          this.resultList = data.data.data.options;
-          // console.log(this.resultList);
-        } catch (error) {
-          this.$toast.fail("搜索失败");
-        }
+    onSearchInput: debounce(async function() {
+      const searchText = this.searchText.trim();
+      if (!searchText) {
+        return;
       }
-      // return this.list;
+      const res = await suggestion(this.searchText);
+      this.resultList = res.data.data.options;
+      console.log(this.resultList)
     }, 300),
     // 高亮
     highlight(val) {
@@ -99,33 +78,20 @@ export default {
       if (!q.trim()) {
         return;
       }
-      // 将搜索的关键字记录到历史记录中
-      let index = this.searchHistory.indexOf(q);
-      // 去重
-      if (index != -1) {
-        this.searchHistory.splice(index, 1);
-      }
-      // 把最近的记录放到最前面
-      this.searchHistory.unshift(q);
-      console.log(this.searchHistory)
-      // 将搜索历史记录放到本次存储，以便持久化
-      setItem("search-histories", this.searchHistory);
+      // // 将搜索的关键字记录到历史记录中
+      // let index = this.searchHistory.indexOf(q);
+      // // 去重
+      // if (index != -1) {
+      //   this.searchHistory.splice(index, 1);
+      // }
+      // // 把最近的记录放到最前面
+      // this.searchHistory.unshift(q);
+      // console.log(this.searchHistory)
+      // // 将搜索历史记录放到本次存储，以便持久化
+      // setItem("search-histories", this.searchHistory);
       this.$router.push(`/search/${q}`);
     },
     onCancel() {},
-    onLoad() {
-      if (this.refreshing) {
-        this.list = [];
-        this.refreshing = false;
-      }
-
-      // 加载状态结束
-      this.loading = false;
-      // 数据全部加载完成
-      if (this.resultList.length >= 40) {
-        this.finished = true;
-      }
-    },
     onRefresh() {
       // 清空列表数据
       this.finished = false;
